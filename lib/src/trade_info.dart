ffd7fdda7cd4103a30abd65d25659d9a402ea1fc
mikedane <mikedane94@gmail.com>
2018-02-01 12:35:30 -0500

    added 'flag' field to TradeInfo

--- lib/src/trade_info.dart
+++ lib/src/trade_info.dart
@@ -10,15 +10,32 @@ class TradeInfo extends ExchangeData{
   /// the [amountTraded].
   double amountPaid;
 
+  /// A flag indicating the nature of the trade.
+  ///
+  /// Supported values are buy, sell and unknown with 1, 2, 3 respectively.
+  int flag;
+  static int BUY_FLAG = 1;
+  static int SELL_FLAG = 2;
+  static int UNKNOWN_FLAG = 3;
+
   TradeInfo.noFields(CoinTradingPair tradingPair, DateTime timestamp )
       : super(tradingPair, timestamp);
 
   TradeInfo(CoinTradingPair tradingPair, DateTime timestamp, this.amountTraded,
-      this.amountPaid) : super(tradingPair, timestamp);
+      this.amountPaid, int flag) : super(tradingPair, timestamp){
+    if(flag != BUY_FLAG || flag != SELL_FLAG || flag != UNKNOWN_FLAG){
+      this.flag = UNKNOWN_FLAG;
+    }
+  }
 
   TradeInfo.fromMap(Map objectMap) : super.fromMap(objectMap){
     if(objectMap["amountTraded"] != null) this.amountTraded = objectMap["amountTraded"];
     if(objectMap["amountPaid"] != null) this.amountPaid = objectMap["amountPaid"];
+    if(objectMap["flag"] != null){
+      this.amountPaid = objectMap["amountPaid"];
+    } else {
+      this.flag = UNKNOWN_FLAG;
+    }
   }
 
   @override
@@ -26,6 +43,7 @@ class TradeInfo extends ExchangeData{
     Map objectMap = {};
     if(this.amountTraded != null) objectMap["amountTraded"] = this.amountTraded;
     if(this.amountPaid != null) objectMap["amountPaid"] = this.amountPaid;
+    if(this.flag != null) objectMap["flag"] = this.flag;
     objectMap.addAll(super.toMap());
     return objectMap;
   }
--- test/trade_info_test.dart
+++ test/trade_info_test.dart
@@ -14,9 +14,10 @@ void testTradeInfoConstructor(){
     DateTime timestamp = new DateTime(2018);
     double amountTraded = 1.0;
     double amountPaid = 2.0;
+    int flag = TradeInfo.BUY_FLAG;
 
     TradeInfo tradeInfo = new TradeInfo(tradingPair, timestamp, amountTraded,
-        amountPaid);
+        amountPaid, flag);
 
     expect(tradeInfo.tradingPair, equals(tradingPair));
     expect(tradeInfo.timestamp, equals(timestamp));
@@ -29,16 +30,17 @@ void testTradeInfoToMap(){
   test("TradeInfo - toMap()", () {
     TradeInfo tradeInfo = new TradeInfo(new CoinTradingPair("btc", "usd", "test_exchange"),
         new DateTime(2018), 1.0,
-        2.0);
+        2.0, TradeInfo.BUY_FLAG);
 
     Map tradeInfoMap = tradeInfo.toMap();
-    expect(tradeInfoMap.length, 6);
+    expect(tradeInfoMap.length, 7);
     expect(tradeInfoMap["timestamp"], equals(tradeInfo.timestamp.millisecondsSinceEpoch));
     expect(tradeInfoMap["baseCoinSymbol"], equals(tradeInfo.tradingPair.baseCoinSymbol));
     expect(tradeInfoMap["quoteCoinSymbol"], equals(tradeInfo.tradingPair.quoteCoinSymbol));
     expect(tradeInfoMap["exchangeName"], equals(tradeInfo.tradingPair.exchangeName));
     expect(tradeInfoMap["amountTraded"], equals(tradeInfo.amountTraded));
     expect(tradeInfoMap["amountPaid"], equals(tradeInfo.amountPaid));
+    expect(tradeInfoMap["flag"], equals(tradeInfo.flag));
 
   });
 }
