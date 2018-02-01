d4707ab56121fe3c21d207734412155bcdc2c7a1
mikedane <mikedane94@gmail.com>
2018-02-01 15:52:00 -0500

    Added 'id' fields to CoinInfo and Exchange

--- lib/src/coin_info.dart
+++ lib/src/coin_info.dart
@@ -1,34 +1,32 @@
 part of crypto_exchange_model;
 
-class CoinInfo extends ExchangeData {
+class CoinInfo {
   String name;
   String symbol;
   String algorithm;
   String proofType;
   int sortOrder;
   String imageUrl;
+  String id;
 
-  CoinInfo(this.name, String symbol, this.algorithm, this.proofType, this.sortOrder,
-      this.imageUrl) : super(new CoinTradingPair(symbol, "NA", "NA"), new DateTime.now()){
-    this.symbol = symbol;
-  }
+  CoinInfo(this.name, this.symbol, this.algorithm, this.proofType, this.sortOrder,
+      this.imageUrl, this.id);
 
-  CoinInfo.nameAndSymbol(this.name, String symbol)
-      : super(new CoinTradingPair(symbol, "NA", "NA"), new DateTime.now()){
-    this.symbol = symbol;
-  }
+  CoinInfo.nameAndSymbol(this.name, this.symbol);
 
-  CoinInfo.fromMap(Map objectMap) : super.fromMap(objectMap) {
+  CoinInfo.nameOnly(this.name);
+
+  CoinInfo.fromMap(Map objectMap) {
     if(objectMap["name"] != null) this.name = objectMap["name"];
     if(objectMap["symbol"] != null) this.symbol = objectMap["symbol"];
     if(objectMap["algorithm"] != null) this.algorithm = objectMap["algorithm"];
     if(objectMap["proofType"] != null) this.proofType = objectMap["proofType"];
     if(objectMap["sortOrder"] != null) this.sortOrder = objectMap["sortOrder"];
     if(objectMap["imageUrl"] != null) this.imageUrl = objectMap["imageUrl"];
+    if(objectMap["id"] != null) this.id = objectMap["id"];
   }
 
 
-  @override
   Map toMap(){
     Map objectMap = {};
     if(this.name != null) objectMap["name"] = this.name;
@@ -37,7 +35,7 @@ class CoinInfo extends ExchangeData {
     if(this.proofType != null) objectMap["proofType"] = this.proofType;
     if(this.sortOrder != null) objectMap["sortOrder"] = this.sortOrder;
     if(this.imageUrl != null) objectMap["imageUrl"] = this.imageUrl;
-    objectMap.addAll(super.toMap());
+    if(this.id != null) objectMap["id"] = this.id;
     return objectMap;
   }
 
