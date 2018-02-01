6e96ce3edff48b9e0e2b7787385773ac6429a2cd
mikedane <mikedane94@gmail.com>
2018-02-01 15:55:16 -0500

    removed Id field from Exchange

--- lib/src/exchange.dart
+++ lib/src/exchange.dart
@@ -8,14 +8,10 @@ class Exchange {
   /// List of coin pairings trading on the exchange.
   List<CoinPair> coinPairs;
 
-  /// Unique identifier for the exchange.
-  String id;
-
-  Exchange(this.name, this.coinPairs, this.id);
+  Exchange(this.name, this.coinPairs);
 
   Exchange.fromMap(Map objectMap){
     if(objectMap["name"] != null) this.name = objectMap["name"];
-    if(objectMap["id"] != null) this.id = objectMap["id"];
     if(objectMap["coinPairs"] != null && objectMap["coinPairs"] is List){
       this.coinPairs = new List<CoinPair>();
       objectMap["coinPairs"].forEach((coinPairMap){
@@ -27,7 +23,6 @@ class Exchange {
   Map toMap(){
     Map objectMap = {};
     if(this.name != null) objectMap["name"] = this.name;
-    if(this.id != null) objectMap["id"] = this.id;
     if(this.coinPairs != null){
       objectMap["coinPairs"] = new List<Map>();
       this.coinPairs.forEach((coinPair){
