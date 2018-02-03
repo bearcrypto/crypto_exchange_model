eb3d37d7887f4d2efbd2c631ea0a01d6e0b172d5
mikedane <mikedane94@gmail.com>
2018-02-03 11:08:01 -0500

    Fixed bug in addExchangeData algorithm

--- lib/src/exchange_data/exchange_data.dart
+++ lib/src/exchange_data/exchange_data.dart
@@ -106,7 +106,7 @@ class ExchangeDataContainer {
     String rt = exchangeData.runtimeType.toString().toLowerCase();
     if(this._exchangeDataMap[b] == null) this._exchangeDataMap[b] = {};
     if(exchangeData is CoinInfo){
-      if(this._exchangeDataMap[b] == null) this._exchangeDataMap[b][rt] = {};
+      if(this._exchangeDataMap[b] == null) this._exchangeDataMap[b] = {};
       this._exchangeDataMap[b] = exchangeData.toMap();
     } else {
       if(this._exchangeDataMap[b][q] == null) this._exchangeDataMap[b][q] = {};
